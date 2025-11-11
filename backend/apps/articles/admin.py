c3da055afc71a28843c43547c3e89fc95c2e34d0
kianibound <kianibound@gmail.com>
2025-11-11 15:58:26 +0330

    Feature: Add Admin filter and search for Articles

--- backend/apps/articles/admin.py
+++ backend/apps/articles/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 from apps.articles.models import Article
-# Register your models here.
 
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'created_at', 'updated_at']
+    search_fields = ['title', 'content']
+    readonly_fields = ['created_at', 'updated_at', 'slug']
+    list_filter = ['created_at']
+    ordering = ['-created_at']
+
+# admin.site.register(Article)
